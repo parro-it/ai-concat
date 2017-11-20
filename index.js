5899f50ca42e39161fab945258e60041578a3160
Andrea Parodi <andrea@parro.it>
2017-11-21 00:28:58 +0100

    bump version of ai-reduce

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import aiReduce from "ai-reduce";
+import reduce from "ai-reduce";
 
 const reducer = (accumulator, item) => accumulator.concat(item);
 
-const concat = aiReduce.partial(reducer, "");
-concat.obj = aiReduce.partial(reducer, []);
-concat.buff = iterable => aiReduce(reducer, [], iterable).then(Buffer.concat);
+const concat = reduce.with(reducer, "");
+concat.obj = reduce.with(reducer, []);
+concat.buff = iterable => reduce(iterable, reducer, []).then(Buffer.concat);
 
 export default concat;
