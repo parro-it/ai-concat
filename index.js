75ac4c2135e9c466818d75bf41370338741999df
Andrea Parodi <andrea@parro.it>
2017-12-04 22:26:31 +0100

    doc

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ import reduce from "ai-reduce";
 
 const reducer = (accumulator, item) => accumulator.concat(item);
 
+/**
+ * Concatenate an async iterable that emits string.
+ * @param {AsyncIterable} iterable - The async iterable to concatenate
+ * @returns {Promise} A promise resolving to a string that is the concatenation of all string emitted.
+ */
 const concat = reduce.with(reducer, "");
+
+/**
+ * Concatenate an async iterable that emits objects.
+ * @param {AsyncIterable} iterable - The async iterable to concatenate
+ * @returns {Promise} A promise resolving to an array containing all objects emitted.
+ */
 concat.obj = reduce.with(reducer, []);
+
+/**
+ * Concatenate an async iterable that emits `Buffer`.
+ * @param {AsyncIterable} iterable - The async iterable to concatenate
+ * @returns {Promise} A promise resolving to a buffer that is the concatenation of all buffers emitted.
+ */
 concat.buff = iterable => reduce(iterable, reducer, []).then(Buffer.concat);
 
 export default concat;
